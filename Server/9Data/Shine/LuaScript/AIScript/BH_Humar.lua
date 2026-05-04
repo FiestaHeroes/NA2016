1d39c7204aa2343e8430f192ad22e4bb567b3357
QuminFlo <epeleptichd@gmail.com>
2026-05-04 12:20:51 +0200

    Update BH_Humar Korean Comments

--- Server/9Data/Shine/LuaScript/AIScript/BH_Humar.lua
+++ Server/9Data/Shine/LuaScript/AIScript/BH_Humar.lua
@@ -1,14 +1,14 @@
 --[[
-���� �������� - �ĸ���
+보스 상위버전 - 후마르
 
-9Data/Here/World/FineScript.txt �ʿ�
-9Data/Hero/Script/BH_Humar.txt �ʿ�
+9Data/Here/World/FineScript.txt 필요
+9Data/Hero/Script/BH_Humar.txt 필요
 ]]
 
--- ��ũ��Ʈ�� ���ϰ�
+-- 스크립트의 리턴값
 ReturnAI = {}
-ReturnAI.END = 1    -- Return_AI_END = 1;//    -- ��� AI��ƾ ��
-ReturnAI.CPP = 2    -- Return_AI_CPP = 2;//    -- ��Ʒ� �Ϻ� ó���� �� cpp�� AI��ƾ ����
+ReturnAI.END = 1    -- Return_AI_END = 1;//    -- 모든 AI루틴 끝
+ReturnAI.CPP = 2    -- Return_AI_CPP = 2;//    -- 루아로 일부 처리한 후 cpp의 AI루틴 돌림
 
 
 --BossIndex = "BH_Humar"
@@ -23,18 +23,18 @@ HumarBuffSec = 60
 SummonSkillName = "BH_Humar_Skill_S"
 SummonSkillCastSecond = 5
 
---��׷� ���Ž� ���� ���ְ� inactiv���·�
+--어그로 제거시 부하 없애고 inactiv상태로
 SummonRate = {}
-SummonRate[1] = 750		-- 75%, 50%, 25%���� �ѹ��� ���ϵ� ����
+SummonRate[1] = 750		-- 75%, 50%, 25%에서 한번씩 부하들 리젠
 SummonRate[2] = 500
 SummonRate[3] = 250
-SummonRate[4] = 0		-- �������� �׻� 0�ΰ� �߰� �ʿ�
+SummonRate[4] = 0		-- 마지막은 항상 0인것 추가 필요
 
 MemBlock = {}
 
 ----------------------------------------------------------------------
 -- Mantis 7984
--- �������� ��ȯ ����
+-- 보물상자 소환 정보
 BH_HumarBox =
 {
 	{ Index = "BH_Humar_X", ItemDropMobIndex = "BH_Humar_O", 	x = 3060, y =  3126, width = 303, height = 494, rotate = 4 },
@@ -42,18 +42,13 @@ BH_HumarBox =
 	{ Index = "BH_Humar_X", ItemDropMobIndex = "BH_Humar_O_03",	x = 3060, y =  3126, width = 303, height = 494, rotate = 4 },
 	{ Index = "BH_Humar_X", ItemDropMobIndex = "BH_Humar_O_04",	x = 3060, y =  3126, width = 303, height = 494, rotate = 4 },
 }
-HumarBox_VanishTime = 60 -- Time for Chests to Despawn.
-HumarBox_ChestsSpawns = 24 -- Chest Amount to Spawn.
 ----------------------------------------------------------------------
-HumarBox_VanishTime = 60 -- When Chests despawn.
-HumarBox_ChestsSpawns = 24 -- How many Chests should spawn.
-
 
 function Common_RemoveTreasure(MapIndex)
 cExecCheck "Common_RemoveTreasure"
 	cDebugLog "Common_RemoveTreasure"
 
-	cRegenGroupActiv("BH_Cracker", "BH_HumarBox", 0)	-- ���ڰ� ������ �ʵ���(����° �μ��� �����ϰų� 1�̸� activ)
+	cRegenGroupActiv("BH_Cracker", "BH_HumarBox", 0)	-- 상자가 나오지 않도록(세번째 인수를 생략하거나 1이면 activ)
 --	cVanishAll(MapIndex, "BH_Humar_O")
 --	cVanishAll(MapIndex, "BH_Humar_O_02")
 --	cVanishAll(MapIndex, "BH_Humar_O_03")
@@ -62,7 +57,7 @@ cExecCheck "Common_RemoveTreasure"
 end
 
 
--- ���ϵ��� ��ũ��Ʈó�� ���� - CPP ��ƾ ���
+-- 부하들은 스크립트처리 안함 - CPP 루틴 사용
 
 ------------------------------------------------------------------
 -----------------------    Humar Part   --------------------------
@@ -72,7 +67,7 @@ function BH_Humar(Handle, MapIndex)
 cExecCheck "BH_Humar"
 	local Var = MemBlock[Handle]
 	if cIsObjectDead(Handle) ~= nil then
-		if Var ~= nil then   -- ������ �׾���
+		if Var ~= nil then   -- 보스가 죽었음
 			cDebugLog("Boss Dead")
 			for k = 1, 2 do
 				if Var.FellowHandle[k] ~= -1 then
@@ -81,9 +76,9 @@ cExecCheck "BH_Humar"
 				end
 			end
 
-			-- �����ΰ� ��ȯ - �������� �����
+			-- 투명인간 소환 - 보물상자 제어용
 			local InvisibleHandle = cMobRegen_Obj("InvisibleMan", Handle)
-			cAIScriptSet(InvisibleHandle, Handle) -- ������ AI��ũ��Ʈ�� Var.Handle(�ﰡ)�� AI��ũ��Ʈ�� ����
+			cAIScriptSet(InvisibleHandle, Handle) -- 비쥬의 AI스크립트를 Var.Handle(헬가)의 AI스크립트로 세팅
 			MemBlock[InvisibleHandle] = {}
 			MemBlock[InvisibleHandle].Handle = InvisibleHandle
 			MemBlock[InvisibleHandle].MapIndex = MapIndex
@@ -95,7 +90,7 @@ cExecCheck "BH_Humar"
 	end
 
 
-	if Var == nil then    -- ó�� �����Ǿ���
+	if Var == nil then    -- 처음 리젠되었음
 		MemBlock[Handle] = {}
 		Var = MemBlock[Handle]
 
@@ -106,10 +101,10 @@ cExecCheck "BH_Humar"
 		Var.FellowHandle[2] = -1
 
 		Var.NextSummonIndex = 1
-		Var.BoxVanish = 0		-- 10�ʿ� �ѹ��� �ڽ��� ����� ����
+		Var.BoxVanish = 0		-- 10초에 한번씩 박스를 지우기 위해
 
-		Var.TargetLostSec = 0 	-- Ÿ���� ���� �ð�
-		Var.NextBuff = 0		-- ������ �������� �ð�
+		Var.TargetLostSec = 0 	-- 타겟을 잃은 시간
+		Var.NextBuff = 0		-- 다음번 버프받을 시간
 
 		Var.Wait = {}
 		Var.Wait.Second = 0;
@@ -121,7 +116,7 @@ cExecCheck "BH_Humar"
 	return Var.StepFunc(Var)
 end
 
-function Boss_Wait(Var)  -- Var.Wait.Second���� ��ٸ� �� StepFunc�� WaitFunction���� ����
+function Boss_Wait(Var)  -- Var.Wait.Second까지 기다린 후 StepFunc을 WaitFunction으로 세팅
 cExecCheck "Boss_Wait"
 	if cCurSec() >= Var.Wait.Second then
 		Var.StepFunc = Var.Wait.NextFunc
@@ -158,7 +153,7 @@ function Boss_BuffCheck(Var)
 cExecCheck "Boss_BuffCheck"
 	hp, maxhp = cObjectHP(Var.Handle)
 	if hp == maxhp then
-		Var.StepFunc = Boss_WaitInvader	-- ���� ��, �� ȸ���Ǿ���
+		Var.StepFunc = Boss_WaitInvader	-- 전투 끝, 피 회복되었음
 		return
 	end
 
@@ -190,7 +185,7 @@ cExecCheck "Boss_SummonCheck"
 		return
 	end
 
-	-- ���ϵ� �׾����� Ȯ��
+	-- 부하들 죽었는지 확인
 	for fel = 1, 2 do
 		if Var.FellowHandle[fel] ~= -1 and cIsObjectDead(Var.FellowHandle[fel]) ~= nil then
 			Var.FellowHandle[fel] = -1
@@ -198,12 +193,12 @@ cExecCheck "Boss_SummonCheck"
 	end
 
 
-	if cTargetHandle(Var.Handle) ~= nil then  -- Ÿ�� ����
+	if cTargetHandle(Var.Handle) ~= nil then  -- 타겟 있음
 		Var.TargetLostSec = cCurSec()
-	elseif Var.TargetLostSec + 10 < cCurSec() then	-- Ÿ�� �����
+	elseif Var.TargetLostSec + 10 < cCurSec() then	-- 타겟 사라짐
 		Var.StepFunc = Boss_WaitInvader
 
-		cResetAbstate(Var.Handle, HumarBuffAbState)	-- ���� ����
+		cResetAbstate(Var.Handle, HumarBuffAbState)	-- 버프 해제
 
 		-- Remove fallows
 		for k = 1, 2 do
@@ -239,7 +234,7 @@ end
 -------------------------------------------------------------------------
 -----------------------    InvisibleMan Part   --------------------------
 -------------------------------------------------------------------------
--- �������� ����� ������Ʈ
+-- 보물상자 제어용 오브젝트
 
 function InvisibleMan(Handle, MapIndex)
 cExecCheck "InvisibleMan"
@@ -296,7 +291,7 @@ cExecCheck "Invisible_AllVanish"
 
 	if cCurSec() > Var.Wait.Second then
 		cDebugLog "Invisible_AllVanish"
-		cRegenGroupActiv("BH_Cracker", "BH_HumarBox", 0)	-- ���ڰ� ������ �ʵ���(����° �μ��� �����ϰų� 1�̸� activ)
+		cRegenGroupActiv("BH_Cracker", "BH_HumarBox", 0)	-- 상자가 나오지 않도록(세번째 인수를 생략하거나 1이면 activ)
 
 		Common_RemoveTreasure(Var.MapIndex)
 		cNPCVanish(Var.Handle)
